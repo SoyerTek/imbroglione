cb7afc4d8762c3b050c9f03f90d6913eac5ab7e2
Raimondo Faggioli <rfaggioli396@gmail.com>
2024-06-25 21:15:41 +0200

    Simpler ui, bugfix whitespaces in name

--- game/views.py
+++ game/views.py
@@ -8,15 +8,17 @@ from game.data import GameManager
 def index(request):#join or create a game
     template = loader.get_template("game/index.html")
     game_id = request.GET.get('game_id', "")
+    gm : GameManager = GameManager.get()
     context = {
         "game_id": game_id,
-        "joining": game_id != ""
+        "joining": game_id != "" and int(game_id) in gm.games 
     }
     return render(request, "game/index.html", context)
 
 def joingame(request):#redirects after adding player to a game or creating a new game
     game_id = int(request.GET.get('game_id', -1))
-    player = request.GET.get('name')
+    player : str = request.GET.get('name')
+    player = player.stip().lower().replace(" ", "_")
     new = game_id == -1
     gm : GameManager = GameManager.get()
     if new:
@@ -41,7 +43,7 @@ def play(request):#play with assigned role in game_id
         "name": player,
         "players": ", ".join(gm.getPlayers(game_id)),
         "round": gm.getRound(game_id),
-        "imbroglione": gm.getImbroglione(game_id) == player,
+        "imbroglione": gm.getImbroglione(game_id).lower() == player.lower(),
         "creator": gm.getCreatingPlayer(game_id) == player,
         "secret_word": gm.getSecret(game_id),
         "suggestion" : gm.getHint(game_id)
